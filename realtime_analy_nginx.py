6be034b3503f0b3f4380ffef2f5bf50be7b826f9
healthy kang <healthly@gmail.com>
2013-09-25 19:39:06 +0800

    fix main

--- realtime_analy_nginx.py
+++ realtime_analy_nginx.py
@@ -8,75 +8,62 @@ from datetime import timedelta
 from pymongo.errors import ConnectionFailure
 _utcnow = datetime.utcnow()
 
-def code404percent(logList):
-	return 404
+def codeOcurrences(logList):
+	
     	
 	
-
-
-def insertR(dbC, collect, data):
-   dbC[collect].insert(data, save=True)
-
-
 def countIP_URL_code(logList):
-   _ip = {}
-   _url = {}
-   _ipp = []
-   _urll = []
-   _code = {}
-   _codee = []
-   for n in logList:
-      _ipp.append(n.get('ip'))
-      _urll.append(n.get('url'))
-      _codee.append(n.get('code'))
-   for m in _ipp:
-      if _ipp.count(m) > 1:
-         _ip[m] = _ipp.count(m)
-   ip = sorted(_ip.items(), key=lambda _ip:_ip[1], reverse=True)
-   print "-----Nginx access IP NUMS in 10 seconds-------"
-   for k in ip:
-      if k[1] > 30:
-         print k[0],':',k[1]
-   
-   for i in _urll:
-      if _urll.count(i) > 1:
-         _url[i] = _urll.count(i)
-   url = sorted(_url.items(), key=lambda _url:_url[1], reverse=True)
-   print "-----Nginx access url NUMs in 10 seconds-------"
-   for j in url:
-      if j[1] > 20:
-         print j[0],':',j[1]
-   
-   for o in _codee:
-      if _codee.count(o) > 1:
-         _code[o] = _codee.count(o)
-   code = sorted(_code.items(), key=lambda _code:_code[1], reverse=True)
-   print "------Nginx codes nums in 10 seconds---------"
-   for p in code:
-      print p[0],':',p[1]
-
+	_ip = {}
+	_url = {}
+	_ipp = []
+	_urll = []
+	_code = {}
+	_codee = []
+	for n in logList:
+		_ipp.append(n.get('ip'))
+		_urll.append(n.get('url'))
+		_codee.append(n.get('code'))
+	for m in _ipp:
+		if _ipp.count(m) > 1:
+			_ip[m] = _ipp.count(m)
+	ip = sorted(_ip.items(), key=lambda _ip:_ip[1], reverse=True)
+	
+	for i in _urll:
+		if _urll.count(i) > 1:
+			_url[i] = _urll.count(i)
+	url = sorted(_url.items(), key=lambda _url:_url[1], reverse=True)
+	
+	for o in _codee:
+		if _codee.count(o) > 1:
+			_code[o] = _codee.count(o)
+	code = sorted(_code.items(), key=lambda _code:_code[1], reverse=True)
+       
+	return (ip,url,code)
+	
+def insertR(dbC, collect, data):
+	dbC[collect].insert(data, save=True)
 
 
 
 def getWebLog(host,port,dbname,times):
-   ''' host: mongodb hostname
-      port: mongodb port
-      dbname: mongodb database names
-      times: times before now to get logs from mongodb,eg:-10
-      '''
-   try:
-      _c1 = Connection(host, port)
-   except ConnectionFailure, e:
-      sys.stderr.write("Could not connect to MongoDB: %s" % e)
-      sys.exit(1)
-   dbC = _c1[dbname]
-   assert dbC.connection == _c1
-   end = _utcnow
-   amin = timedelta(seconds=times)
-   #amin = timedelta(minutes=-1)
-   start = end + amin
-   _r1 = dbC.access.find({'time': {'$gte': start, '$lt': end}})
-   return _r1
+	''' host: mongodb hostname
+	port: mongodb port
+	dbname: mongodb database names
+	times: times before now to get logs from mongodb,eg:-10
+	'''
+	try:
+		_c1 = Connection(host, port)
+	except ConnectionFailure, e:
+		sys.stderr.write("Could not connect to MongoDB: %s" % e)
+		sys.exit(1)
+	dbC = _c1[dbname]
+	assert dbC.connection == _c1
+	end = _utcnow
+	amin = timedelta(seconds=times)
+	#amin = timedelta(minutes=-1)
+	start = end + amin
+	_r1 = dbC.access.find({'time': {'$gte': start, '$lt': end}})
+	return _r1
 
 
 
@@ -122,22 +109,42 @@ def getLogItems(weblog,*args):
             logOut.append(_logD)
    return (logIn,logOut)
 
-def main():
+def argP():
 	parser = argparse.ArgumentParser(description='analy nginx log from mongodb')
 	parser.add_argument('-H', dest="dbhost", default='localhost', help="mongodb's ip or hostname")
 	parser.add_argument('-N', dest="dbname", default='nginx111', help="mongodb's dbname")
 	parser.add_argument('--ipurlcode', dest="ipurlcode", action='store_true', help="output out ip ,urls ,codes")
 	#parser.add_argument('-N', dest="dbname", type=int)
-   
-	args = parser.parse_args()
-	if args.ipurlcode:
-      i = getWebLog(args.dbhost,27017,args.dbname,-10)
-      m = getLogItems(i,u'method',u'referer',u'code',u'size',u'agent')[1]
-      countIP_URL_code(m)
+	
+	return parser
+	
+	
+def main():
+	argS = argP().parse_args()
+	if argS.ipurlcode:
+		i = getWebLog(argS.dbhost,27017,argS.dbname,-10)
+		m = getLogItems(i,u'method',u'referer',u'code',u'size',u'agent')[1]
+		
+		ip = countIP_URL_code(m)[0]
+		print "-----Nginx access IP NUMS in 10 seconds-------"
+		for k in ip:
+			if k[1] > 30:
+				print k[0],':',k[1]
+		
+		url = countIP_URL_code(m)[1]
+		print "-----Nginx access url NUMs in 10 seconds-------"
+		for j in url:
+			if j[1] > 20:
+				print j[0],':',j[1]
+		
+		code = countIP_URL_code(m)[2]
+		print "------Nginx codes nums in 10 seconds---------"
+		for p in code:
+			print p[0],':',p[1]
 	else:
-      parser.print_help()
-#c1 = u'nginx1'
-#insertR(dbC, c1, logIn)
+		argP().print_help()
+	#c1 = u'nginx1'
+	#insertR(dbC, c1, logIn)
 
 
 if __name__ == "__main__":
