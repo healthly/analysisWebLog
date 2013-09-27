ef7e658ef52d8ee263046d61c5e3468f076ab2c4
healthy kang <healthly@gmail.com>
2013-09-27 09:14:45 +0800

    modfiy argparse & output

--- realtime_analy_nginx.py
+++ realtime_analy_nginx.py
@@ -23,23 +23,23 @@ def c_m_Ocurrences(logList):
 		if _meths.count(m) > 0:
 			_meth[m] = _meths.count(m)
 	meth = sorted(_meth.items(), key=lambda _meth:_meth[1], reverse=True)
-	print "------Nginx methods nums in 10 seconds---------"
+	print "------Nginx methods nums in 20 seconds---------"
 	print 'total nums :' + str(_total)
 	for p in meth:
-			per = int(p[1]) / float(_total) * 100
-			per = float('%0.3f' % per)
-			print p[0],':',p[1],':',str(per) + '%'
+		per = int(p[1]) / float(_total) * 100
+		per = float('%0.3f' % per)
+		print p[0],':',p[1],':',str(per) + '%'
 			
 	for m in _codes:
 		if _codes.count(m) > 0:
 			_code[m] = _codes.count(m)
 	code = sorted(_code.items(), key=lambda _code:_code[1], reverse=True)
-	print "------Nginx codes nums in 10 seconds---------"
+	print "------Nginx codes nums in 20 seconds---------"
 	print 'total nums :' + str(_total)
 	for p in code:
-			per = int(p[1]) / float(_total) * 100
-			per = float('%0.3f' % per)
-			print p[0],':',p[1],':',str(per) + '%'
+		per = int(p[1]) / float(_total) * 100
+		per = float('%0.3f' % per)
+		print p[0],':',p[1],':',str(per) + '%'
 			
 def countIP_URL(logList):
 	_ip = {}
@@ -48,6 +48,7 @@ def countIP_URL(logList):
 	_urll = []
 	_code = {}
 	_codee = []
+	_total = len(logList)
 	for n in logList:
 		_ipp.append(n.get('ip'))
 		_urll.append(n.get('url'))
@@ -62,12 +63,14 @@ def countIP_URL(logList):
 			_url[i] = _urll.count(i)
 	url = sorted(_url.items(), key=lambda _url:_url[1], reverse=True)
 	
-	print "-----Nginx access IP NUMS in 10 seconds-------"
+	print "-----Nginx access IP NUMS in 20 seconds-------"
+	print 'total nums :' + str(_total)
 	for k in ip:
 		if k[1] > 30:
 			print k[0],':',k[1]
 		
-	print "-----Nginx access url NUMs in 10 seconds-------"
+	print "-----Nginx access url NUMs in 20 seconds-------"
+	print 'total nums :' + str(_total)
 	for j in url:
 		if j[1] > 20:
 			print j[0],':',j[1]
@@ -144,10 +147,10 @@ def getLogItems(weblog,*args):
 
 def argP():
 	parser = argparse.ArgumentParser(description='analy nginx log from mongodb')
-	parser.add_argument('-H', dest="dbhost", default='localhost', help="mongodb's ip or hostname")
-	parser.add_argument('-N', dest="dbname", default='nginx111', help="mongodb's dbname")
 	parser.add_argument('--ipurl', dest="ipurl", action='store_true', help="output out ips ,urls in ten seconds")
 	parser.add_argument('--codemethod', dest="codemethod", action='store_true', help="output out codes,methods Ocurrences in ten seconds")
+	parser.add_argument('-H', dest="dbhost", default='localhost', help="mongodb's ip or hostname")
+	parser.add_argument('-N', dest="dbname", default='nginx111', help="mongodb's dbname")
 	#parser.add_argument('-N', dest="dbname", type=int)
 	
 	return parser
@@ -157,17 +160,20 @@ def main():
 	argS = argP().parse_args()
 	if len(sys.argv) <= 1:
 		argP().print_help()
-		return 1
 	
 	else:
-		i = getWebLog(argS.dbhost,27017,argS.dbname,-10)
-		m = getLogItems(i,u'method',u'referer',u'code',u'size',u'agent')[1]
+		 
 		if argS.ipurl:
+			i = getWebLog(argS.dbhost,27017,argS.dbname,-20)
+			m = getLogItems(i,u'method',u'referer',u'code',u'size',u'agent')[1]
 			countIP_URL(m)
 		
 		elif argS.codemethod:
+			i = getWebLog(argS.dbhost,27017,argS.dbname,-20)
+			m = getLogItems(i,u'method',u'referer',u'code',u'size',u'agent')[1]
 			c_m_Ocurrences(m)
-		
+		else:
+			argP().print_help()
 	#c1 = u'nginx1'
 	#insertR(dbC, c1, logIn)
 
